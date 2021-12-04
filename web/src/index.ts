b1ecfbab0037474bd73f7108d806e45a63018b86
tjjfvi <tjjfvi@tjjfvi.com>
2021-12-04 18:21:05 -0500

    ...

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,78 +5,83 @@ import { Delaunay } from "d3-delaunay"
 console.log(rs.hi())
 
 const size = 750
-const cellSize = 30
-const cellVariance = 2
+const cellVariance = 1
 
 let canvas = document.getElementById("canvas") as HTMLCanvasElement
 let ctx = canvas.getContext("2d")!
-function makeVoronoi(){
-  const points = [...Array(Math.ceil(size / cellSize))].flatMap((_, i) => [...Array(Math.ceil(size / cellSize))].map((_, j) => genPoint(i, j)))
+function makeVoronoi(f: (x: [number, number]) => boolean, cellSize: number){
+  const points = [...Array(Math.ceil(size / cellSize))].flatMap((_, i) => [...Array(Math.ceil(size / cellSize))].map((_, j) => genPoint(i, j))).filter(f)
   const delaunay = Delaunay.from(points)
   const voronoi = delaunay.voronoi([0, 0, size, size])
   return voronoi
+  function genPoint(cellX: number, cellY: number){
+    return [
+      (Math.random() - .5) * cellSize * cellVariance + cellX * cellSize,
+      (Math.random() - .5) * cellSize * cellVariance + cellY * cellSize,
+    ] as [number, number]
+  }
+
 }
 
-
-function genPoint(cellX: number, cellY: number){
-  return [
-    (Math.random() - .5) * cellSize * cellVariance + cellX * cellSize,
-    (Math.random() - .5) * cellSize * cellVariance + cellY * cellSize,
-  ]
-}
 
 function tick(){
   // window.requestAnimationFrame(tick)
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
-  ctx.fillStyle = "white"
+  ctx.fillStyle = "red"
   ctx.fillRect(0, 0, canvas.width, canvas.height)
-  ctx.fillStyle = "black"
-  text("A", 0, 0, size, size)
+  ctx.fillStyle = "green"
+  ctx.strokeStyle = "blue"
+  ctx.lineWidth = 5
+  ctx.stroke
+  text("stroke", "A", 0, 0, size, size)
+  text("fill", "A", 0, 0, size, size)
   let mask = ctx.getImageData(0, 0, size, size)
-  setTimeout(() => {
-    let voronoi = makeVoronoi()
+  let vs = []
+  for(let [color, text, cellSize] of [["#0000ff", false, 70], ["#9999ff", true, 45]]as const) {
+    ctx.putImageData(mask, 0, 0)
+    let voronoi = makeVoronoi(x => (ctx.getImageData(x[0] | 0, x[1] | 0, 1, 1).data[0] === 0) === text, cellSize)
     let delaunay = voronoi.delaunay
-    let x = []
-    for(const cell of voronoi.cellPolygons()) {
-      let y = 0
-      for(const point of [...cell, [delaunay.points[cell.index * 2], delaunay.points[cell.index * 2 + 1]]])
-        if(ctx.getImageData(point[0] | 0, point[1] | 0, 1, 1).data.join(",") === "0,0,0,255") {
-          y++
-        }
-      x.push(y > 2)
-    }
     ctx.fillStyle = "white"
+    ctx.lineCap = "round"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "blue"
-    console.log(x.length, [...voronoi.cellPolygons()].length)
-    console.log(ctx.getImageData(413, 472, 1, 1).data)
     for(const cell of voronoi.cellPolygons()) {
-    // if(Math.random() > .1) continue
-      ctx.strokeStyle = "white"
-      ctx.lineWidth = 5
+      ctx.strokeStyle = "#eee"
+      // ctx.strokeStyle = "white"
+      // ctx.lineWidth = 4
       ctx.beginPath()
       voronoi.renderCell(cell.index, ctx)
-      // ctx.lineTo(delaunay.points[cell.index * 2], delaunay.points[cell.index * 2 + 1])
-      // ctx.lineTo(413, 472)
       ctx.stroke()
-      ctx.fillStyle = x.shift() ? "blue" : "lightblue"
+      ctx.fillStyle = color
       ctx.fill()
       ctx.closePath()
     }
-  }, 10)
+    vs.push(ctx.getImageData(0, 0, size, size))
+  }
+  for(let i = 0; i < mask.data.length; i++) {
+    let R = vs[0].data.slice(i * 4, i * 4 + 4)
+    let G = vs[1].data.slice(i * 4, i * 4 + 4)
+    let B = [0xee, 0xee, 0xee, 255]
+    // B = [255, 255, 255, 255]
+    let [r, g, b] = mask.data.slice(i * 4, i * 4 + 4)
+    let m = r + g + b
+    for(let j = 0; j < 4; j++)
+      mask.data[i * 4 + j] = R[j] * r / m + G[j] * g / m + B[j] * b / m
+  }
+  ctx.putImageData(mask, 0, 0)
 }
 
-function text(text: string, x: number, y: number, width: number, height: number){
+function text(mode: "fill" | "stroke", text: string, x: number, y: number, width: number, height: number){
   let size = measure(text)
   let scale = Math.min(height / size[1], width / size[0])
   let rw = scale * size[0]
   let rh = scale * size[1]
   console.log(size, width, height, rw, rh)
   // ctx.fillRect(x + (width - rw) / 2, y + (height - rh) / 2, rw, rh)
-  ctx.font = `${scale}px Inconsolata`
+  ctx.font = `bold ${scale}px Inconsolata`
   ctx.textBaseline = "middle"
-  ctx.fillText(text, x + (width - rw) / 2, y + height / 2)
+  ctx[`${mode}Text` as const](text, x + (width - rw) / 2, y + height / 2)
 }
 
 let measureMemo: Record<string, readonly [number, number]> = {}
