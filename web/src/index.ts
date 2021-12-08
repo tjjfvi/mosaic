ab67e49c29f72831bb67d74c2631bbd1ea6edbc6
tjjfvi <tjjfvi@tjjfvi.com>
2021-12-07 21:44:30 -0500

    ...

--- web/src/index.ts
+++ web/src/index.ts
@@ -33,13 +33,20 @@ function tick(){
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 
+  const lowRes = new t.Group()
+  const highRes = new t.Group()
+  scene.add(lowRes, highRes)
+
+  for(let i = 0; i < 5; i++)
+    for(let j = 0; j < 5; j++) {
+      let tile = createTile("#03f", "#0f3", "@")
+      lowRes.add(lowResTile(tile, i, j))
+      highRes.add(highResTile(tile, i, j))
+    }
+  //     createTile("#03f", "#0f3", "@", i, j)
   // for(let i = 0; i < 20; i++)
   //   for(let j = 0; j < 20; j++)
   //     createTile("#03f", "#0f3", "@", i, j)
-  for(let i = 0; i < 20; i++)
-    for(let j = 0; j < 20; j++)
-      createTile("#03f", "#0f3", "@", i, j)
-  // createTile("#03f", "#0f3", "@", 0, 0)
 
   camera.position.set(0, 50, 0)
 
@@ -65,24 +72,30 @@ function tick(){
   function f(){
     window.requestAnimationFrame(f)
     trackballControls.update()
-    orbitControls.minPolarAngle = orbitControls.maxPolarAngle = Math.PI / 2 *  (1 - orbitControls.getDistance() / 150) ** 2
+    const dist = orbitControls.getDistance()
+    orbitControls.minPolarAngle = orbitControls.maxPolarAngle = Math.PI / 2 *  (1 - dist / 150) ** 2
     orbitControls.update()
+    highRes.visible = dist < 30
+    lowRes.visible = dist > 30
     renderer.render(scene, camera)
   }
 }
-function createTile(bgColor: string, fgColor: string, symbol: string, x:number, y: number){
+
+interface Stone {
+  poly: Point[],
+  color: t.Color,
+  roughness: number,
+  metalness: number,
+  slantDir: number,
+  slantSlope: number,
+  center: Point,
+}
+
+const tileSize = 10
+const thickness = .1
+
+function createTile(bgColor: string, fgColor: string, symbol: string): Stone[]{
   let symbolPoly = getSymbolPolygon(symbol, size)
-
-  const tileSize = 10
-
-  interface Stone {
-    poly: Point[],
-    color: t.Color,
-    roughness: number,
-    metalness: number,
-    slantDir: number,
-    slantSlope: number,
-  }
 
   const stones: Stone[] = []
 
@@ -106,8 +119,7 @@ function createTile(bgColor: string, fgColor: string, symbol: string, x:number, 
           let max = g.reduce((a, b) => [Math.max(a[0], b[0]), Math.max(a[1], b[1])])
           let center = [min[0] / 2 + max[0] / 2, min[1] / 2 + max[1] / 2] as const
           let slantDir = Math.random() * Math.PI * 2
-          const thickness = .1
-          let slantAmount = Math.random() * thickness * 1
+          let slantAmount = Math.random() * thickness
           let slantSlope = slantAmount / (v.mag(v.sub(max, min)) * tileSize / size)
 
           stones.push({
@@ -117,12 +129,17 @@ function createTile(bgColor: string, fgColor: string, symbol: string, x:number, 
             metalness: .05 + Math.random() * .05,
             slantDir,
             slantSlope,
+            center,
           })
         }
       }
     }
   }
 
+  return stones
+}
+
+function lowResTile(stones: Stone[], x: number, y: number){
   const textureSize = 500
 
   const createTexture = () => {
@@ -151,7 +168,7 @@ function createTile(bgColor: string, fgColor: string, symbol: string, x:number, 
     albedo.fillStyle = rgb(stone.color.r, stone.color.g, stone.color.b)
     surface.fillStyle = rgb(0, stone.roughness, stone.metalness)
     const h = Math.sqrt(1 / stone.slantSlope ** 2 + 1)
-    let x = [-Math.cos(stone.slantDir) * .5 / h + .5, -Math.sin(stone.slantDir) * .5 / h + .5, 1 / stone.slantSlope * .5 / h + .5] as const
+    let x = [Math.cos(stone.slantDir) * .5 / h + .5, -Math.sin(stone.slantDir) * .5 / h + .5, 1 / stone.slantSlope * .5 / h + .5] as const
     normal.fillStyle = rgb(...x)
     for(const ctx of [albedo, surface, normal]) {
       ctx.beginPath()
@@ -159,11 +176,10 @@ function createTile(bgColor: string, fgColor: string, symbol: string, x:number, 
       for(let p of stone.poly)
         ctx.lineTo(...v.scl(p, textureSize / size))
       ctx.fill()
+      ctx.fill()
       ctx.closePath()
     }
   }
-
-
 
   const geo = new t.PlaneGeometry(tileSize, tileSize)
   const mat = new t.MeshStandardMaterial({ transparent: true })
@@ -172,92 +188,95 @@ function createTile(bgColor: string, fgColor: string, symbol: string, x:number, 
   mat.metalnessMap = surfaceMap
   mat.normalMap = normalMap
   mat.normalMapType = t.ObjectSpaceNormalMap
-  const plane = new t.Mesh(geo, mat)
-  plane.rotation.x = -Math.PI / 2
-  plane.position.set(x * tileSize, 0, y * tileSize)
-  scene.add(plane)
 
-  renderer.render(scene, camera)
+  const tile = new t.Mesh(geo, mat)
+  tile.rotation.x = -Math.PI / 2
+  tile.position.set(x * tileSize, thickness, y * tileSize)
+
+  return tile
 
   function rgb(r: number, g: number, b: number){
     return `rgb(${r * 256 | 0}, ${g * 256 | 0}, ${b * 256 | 0})`
   }
 }
 
-function bspGeo(bsp: Bsp, s: number): t.BufferGeometry{
-  const geometry = new t.BufferGeometry()
+function highResTile(stones: Stone[], x: number, y: number){
+  const tile = new t.Group()
+  for(let stone of stones) {
 
-  let edges = [...bspEdges(bsp)]
-  let polys = [...bspToConvexPolygons(bsp, [])]
+    let edges = allPolyEdges([stone.poly])
+    let polys = [...bspToConvexPolygons(polygonToBsp([stone.poly]), [])]
 
-  const thickness = .1
+    let slantDirV = v.scl([Math.cos(stone.slantDir), Math.sin(stone.slantDir)], stone.slantSlope * tileSize / size)
 
-  let min = edges.flat().reduce((a, b) => [Math.min(a[0], b[0]), Math.min(a[1], b[1])])
-  let max = edges.flat().reduce((a, b) => [Math.max(a[0], b[0]), Math.max(a[1], b[1])])
-  let center = [min[0] / 2 + max[0] / 2, min[1] / 2 + max[1] / 2] as const
-  let slantDir = Math.random() * Math.PI * 2
-  let slantAmount = Math.random() * thickness * 1
-  let slantDirV = v.scl([Math.cos(slantDir), Math.sin(slantDir)], slantAmount / v.mag(v.sub(max, min)))
+    let th = (x: number, y: number) =>
+      thickness - v.dot(slantDirV, v.sub([x, y], stone.center))
 
-  let th = (x: number, y: number) =>
-    thickness - v.dot(slantDirV, v.sub([x, y], center))
+    const topGeo = makeGeo(
+      polys.flatMap(poly => poly.length > 2 ? [...Array(poly.length - 2)].flatMap((_, i) =>
+        [
+          poly[i + 1],
+          poly[0],
+          poly[i + 2],
+        ].map(([x, y]) => [x * tileSize / size, th(x, y), y * tileSize / size]),
+      ) : []),
+    )
 
-  const vertices = new Float32Array(
-    polys.map(x => (x.length - 2) * 3 * 3).reduce((a, b) => a + b, 0) * 2
-    + edges.length * 2 * 3 * 3,
-  )
+    const sideGeo = makeGeo(
+      edges.flatMap(edge =>
+        ([
+          [edge[1], 1],
+          [edge[0], 1],
+          [edge[1], 0],
+          [edge[0], 1],
+          [edge[0], 0],
+          [edge[1], 0],
+        ] as const).map(([[x, y], z]) =>
+            [x * tileSize / size, z * th(x, y), y * tileSize / size] as const,
+        ),
+      ),
+    )
 
-  let vi = 0
+    const topMat = new t.MeshStandardMaterial({
+      color: stone.color,
+      roughness: stone.roughness,
+      metalness: stone.metalness,
+    })
 
-  for(let poly of polys)
-    for(let i = 0; i < poly.length - 2; i++)
-      for(let [[x, y], z] of [
-        [poly[0], 0],
-        [poly[i + 1], 0],
-        [poly[i + 2], 0],
-        [poly[i + 1], 1],
-        [poly[0], 1],
-        [poly[i + 2], 1],
-      ] as const) {
-        vertices[vi++] = x * s
-        vertices[vi++] = z * th(x, y)
-        vertices[vi++] = y * s
-      }
+    const sideColor = new t.Color(stone.color)
+    let hsl = { h: 0, s: 0, l: 0 }
+    sideColor.getHSL(hsl)
+    sideColor.setHSL(hsl.h, hsl.s * .7, hsl.l * .5)
+    const sideMat = new t.MeshStandardMaterial({
+      color: sideColor,
+    })
 
-  for(let edge of edges)
-    for(let [[x, y], z] of [
-      [edge[1], 1],
-      [edge[0], 1],
-      [edge[1], 0],
-      [edge[0], 1],
-      [edge[0], 0],
-      [edge[1], 0],
-    ] as const) {
-      vertices[vi++] = x * s
-      vertices[vi++] = z * th(x, y)
-      vertices[vi++] = y * s
-    }
-
-  geometry.setAttribute("position", new t.BufferAttribute(vertices, 3))
-  return geometry
+    const topMesh = new t.Mesh(topGeo, topMat)
+    const sideMesh = new t.Mesh(sideGeo, sideMat)
+    const group = new t.Group()
+    group.add(topMesh, sideMesh)
+    group.position.set((x - .5) * tileSize, 0, (y - .5) * tileSize)
+    tile.add(group)
+  }
+  return tile
 }
 
-// function strokePolygon(poly: Point[]){
-//   ctx.beginPath()
-//   ctx.moveTo(...poly[poly.length - 1])
-//   for(const p of poly)
-//     ctx.lineTo(...p)
-//   ctx.stroke()
-//   ctx.closePath()
-// }
-
-// function fillPolygon(poly: Point[]){
-//   ctx.beginPath()
-//   ctx.moveTo(...poly[poly.length - 1])
-//   for(const p of poly)
-//     ctx.lineTo(...p)
-//   ctx.fill()
-//   ctx.closePath()
-// }
-
 tick()
+
+function makeGeo(verts: (readonly [number, number, number])[]){
+  const geometry = new t.BufferGeometry()
+  const vertices = new Float32Array(verts.length * 3)
+
+  let i = 0
+
+  for(let v of verts) {
+    vertices[i++] = v[0]
+    vertices[i++] = v[1]
+    vertices[i++] = v[2]
+  }
+
+  geometry.setAttribute("position", new t.BufferAttribute(vertices, 3))
+  geometry.computeVertexNormals()
+
+  return geometry
+}
